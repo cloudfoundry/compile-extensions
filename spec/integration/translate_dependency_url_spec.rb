bc45964b9bc60ffe9f42d1b24b2df7c4c71cc106
Rasheed Abdul-Aziz and Sai To Yeung <pair+squeedee+syeung@pivotal.io>
2014-12-08 16:56:04 -0500

    use manifest.yml to translate URI, without name/version key
    [#83883822]

--- spec/integration/translate_dependency_url_spec.rb
+++ spec/integration/translate_dependency_url_spec.rb
@@ -1,35 +1,74 @@
 require 'spec_helper'
+require 'open3'
 
 describe 'translate_dependency_url' do
   def run_translate
     binary_file = File.expand_path('../../../bin/translate_dependency_url', __FILE__)
-    `cd #{buildpack_dir} && #{binary_file} #{original_url}`
+    Open3.capture3("cd #{buildpack_dir} && #{binary_file} #{original_url}")
   end
 
   let(:buildpack_dir) { Dir.mktmpdir }
-  let(:original_url) { 'http://thing.com/file.txt' }
+
+  let(:manifest) {
+    <<-MANIFEST
+---
+dependencies:
+  -
+    original: http://thing.com/file.txt
+    xlated: http://thong.co.nz/file.txt
+  -
+    original: http://place.net/a_file.zip
+    xlated: https://another_location/a_file_elsewhere.zip
+    MANIFEST
+  }
+
+  before do
+    File.open(File.join(buildpack_dir, 'manifest.yml'), 'w') do |file|
+      file.puts manifest
+    end
+  end
 
   after do
     FileUtils.remove_entry buildpack_dir
   end
 
   context 'with a cache' do
-    before do
-      `mkdir #{buildpack_dir}/dependencies`
-    end
+    context 'when the url is defined in the manifest' do
+      let(:original_url) { 'http://place.net/a_file.zip' }
 
-    specify do
-      translated_url = run_translate
+      before do
+        `mkdir #{buildpack_dir}/dependencies`
+      end
 
-      expect(translated_url).to eq("file://#{buildpack_dir}/dependencies/http___thing.com_file.txt\n")
+      specify do
+        translated_url, _, _ = run_translate
+
+        expect(translated_url).to eq("file://#{buildpack_dir}/dependencies/https___another_location_a_file_elsewhere.zip\n")
+      end
     end
   end
 
   context 'without a cache' do
-    specify do
-      translated_url = run_translate
+    context 'when the url is defined in the manifest' do
+      let(:original_url) { 'http://thing.com/file.txt' }
 
-      expect(translated_url).to eq "#{original_url}\n"
+      specify do
+        translated_url, _, _ = run_translate
+
+        expect(translated_url).to eq "http://thong.co.nz/file.txt\n"
+      end
+    end
+
+    context 'when the url is not defined in the manifest' do
+      let(:original_url) { 'http://i_r.not/here' }
+
+      specify do
+        translated_url, _, status = run_translate
+
+        expect(translated_url).to eq "DEPENDENCY_MISSING_IN_MANIFEST: #{original_url}\n"
+        expect(status).not_to be_success
+      end
+
     end
   end
 end
