f3fe2c89e34f30c6aa7c8caec55fe064b5b3fde1
JT Archie and Jacques Chester <pair+jarchie+jchester@pivotal.io>
2014-12-15 15:14:43 -0500

    fix failing test [#84611650]

--- spec/integration/is_cached_spec.rb
+++ spec/integration/is_cached_spec.rb
@@ -4,11 +4,11 @@ describe 'is_cached command line predicate' do
   let(:buildpack_dir) { Dir.mktmpdir }
 
   before do
-    ENV['BUILDPACK_PATH'] = tmp_dir
+    ENV['BUILDPACK_PATH'] = buildpack_dir
   end
 
   after do
-    FileUtils.remove_entry tmp_dir
+    FileUtils.remove_entry buildpack_dir
     ENV.delete('BUILDPACK_PATH')
   end
 
@@ -21,7 +21,7 @@ describe 'is_cached command line predicate' do
 
   context 'BUILDPACK_PATH does not have a dependencies folder' do
     before do
-      Dir.mkdir(File.join(tmp_dir, "dependencies"))
+      Dir.mkdir(File.join(buildpack_dir, "dependencies"))
     end
 
     specify do
