fb3653aa7e7d9824827f06cd9b18a05768539b23
Brandon Shroyer <bshroyer@pivotal.io>
2016-01-15 10:20:05 -0500

    Force version input for `highest_version` to follow Godeps format
    [#106117500]
    
    Signed-off-by: James Wen <jrw2175@columbia.edu>

--- spec/integration/highest_version_spec.rb
+++ spec/integration/highest_version_spec.rb
@@ -14,19 +14,19 @@ describe 'highest_version' do
   end
 
   context 'use semantically valid exact match' do
-    let (:version) { '1.5.1' }
+    let (:version) { 'go1.5.2' }
     let (:dependency) { 'go' }
     let(:manifest_path) { File.join(@base_dir, 'spec', 'fixtures', 'all_patch_level_manifest.yml') }
 
     it 'returns an exact match of the selected version' do
       highest_version, _, status = run_highest_version(manifest_path, dependency, version)
-      expect(highest_version).to eq '1.5.1'
+      expect(highest_version).to eq '1.5.2'
       expect(status).to eq(0)
     end
   end
 
   context 'use semantically valid version with no exact match' do
-    let (:version) { '1.5.1' }
+    let (:version) { 'go1.5.2' }
     let (:dependency) { 'go' }
     let(:manifest_path) { File.join(@base_dir, 'spec', 'fixtures', 'only_lts_manifest.yml') }
 
@@ -38,21 +38,21 @@ describe 'highest_version' do
   end
 
   context 'use wildcard without an exact major-minor match' do
-    let (:version) { '1.5' }
+    let (:version) { 'go1.5' }
     let (:dependency) { 'go' }
     let(:manifest_path) { File.join(@base_dir, 'spec', 'fixtures', 'all_patch_level_manifest.yml') }
 
     it 'returns an exact match of the selected version' do
       highest_version, _, status = run_highest_version(manifest_path, dependency, version)
 
-      expect(highest_version).to eq '1.5.2'
+      expect(highest_version).to eq '1.5.3'
       expect(status).to eq(0)
     end
   end
 
   context 'use wildcard with an exact major-minor match and no patch level' do
 
-    let (:version) { '1.5' }
+    let (:version) { 'go1.5' }
     let (:dependency) { 'go' }
     let(:manifest_path) { File.join(@base_dir, 'spec', 'fixtures', 'one_missing_patch_manifest.yml') }
     it 'returns highest minor version for specified major version' do
@@ -63,7 +63,7 @@ describe 'highest_version' do
   end
 
   context 'use a wildcard match on an LTS version when a newer version is present' do
-    let (:version) { '1.4' }
+    let (:version) { 'go1.4' }
     let (:dependency) { 'go' }
     let(:manifest_path) { File.join(@base_dir, 'spec', 'fixtures', 'manifest.yml') }
 
@@ -76,15 +76,30 @@ describe 'highest_version' do
   end
 
   context 'use a wildcard match when no major-minor matches present' do
-    let (:version) { '1.5' }
+    let (:version) { 'go1.5' }
     let (:dependency) { 'go' }
     let(:manifest_path) { File.join(@base_dir, 'spec', 'fixtures', 'only_lts_manifest.yml') }
 
-    it 'returns an exact match of the selected version' do
+    it 'returns a blank version match' do
       highest_version, _, status = run_highest_version(manifest_path, dependency, version)
 
       expect(highest_version).to eq ''
       expect(status).to eq(0)
     end
   end
+
+  context 'use a wildcard match when version is not prefixed with go' do
+    let (:version) { '1.5' }
+    let (:dependency) { 'go' }
+    let(:manifest_path) { File.join(@base_dir, 'spec', 'fixtures', 'only_lts_manifest.yml') }
+
+    it 'does not return a version match and fails' do
+      highest_version, err_message, status = run_highest_version(manifest_path, dependency, version)
+
+      expect(highest_version).to eq ''
+      expect(err_message).to include "invalid Godeps version '1.5'"
+      expect(status.exitstatus).to eq(1)
+    end
+  end
+
 end
