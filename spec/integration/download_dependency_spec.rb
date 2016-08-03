40e9d1571821b1858112ec30fd8ad587bcf9b3af
Sam Smith <sesmith177@gmail.com>
2016-08-03 11:15:14 -0400

    Make run_download_dependency params explicit in download_dependency spec
    - Clean up formatting
    
    [#127357631]
    
    Signed-off-by: James Wen <jrw2175@columbia.edu>

--- spec/integration/download_dependency_spec.rb
+++ spec/integration/download_dependency_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 require 'open3'
 
 describe 'download_dependency' do
-  def run_download_dependency(input = file_path)
+  def run_download_dependency(input)
     Open3.capture3("#{buildpack_directory}/compile-extensions/bin/download_dependency #{input} #{install_directory}")
   end
 
-  let(:credentials)  { 'login:password' }
-  let(:redacted)     { '-redacted-:-redacted-' }
-  let(:proxy) { Billy::Proxy.new }
-  let(:buildpack_directory) { Dir.mktmpdir }
-  let(:install_directory) { Dir.mktmpdir }
+  let(:credentials)          { 'login:password' }
+  let(:redacted)             { '-redacted-:-redacted-' }
+  let(:proxy)                { Billy::Proxy.new }
+  let(:buildpack_directory)  { Dir.mktmpdir }
+  let(:install_directory)    { Dir.mktmpdir }
+  let(:dependency_file_name) { 'something.txt' }
   let!(:manifest) do
     File.open(File.join(buildpack_directory, 'manifest.yml'), 'w') do |file|
       file.write <<-MANIFEST
@@ -34,13 +35,12 @@ dependencies:
   end
 
   let(:file_path) do
-    path = File.join(Dir.mktmpdir, 'something.txt')
+    path = File.join(Dir.mktmpdir, dependency_file_name)
     File.write(path, 'something')
     path
   end
   let(:manifest_url) { "file://#{file_path}" }
-
-  let(:md5) { Digest::MD5.file(file_path).hexdigest }
+  let(:md5)          { Digest::MD5.file(file_path).hexdigest }
 
   before do
     base_dir = File.expand_path(File.join(File.dirname(__FILE__), "..", ".."))
@@ -63,20 +63,20 @@ dependencies:
     end
 
     it 'displays the translated uri to STDOUT' do
-        stdout, _, _ = run_download_dependency(manifest_url)
-        expect(stdout.chomp).to include("file://#{cached_file_location}")
+      stdout, _, _ = run_download_dependency(manifest_url)
+      expect(stdout.chomp).to include("file://#{cached_file_location}")
     end
   end
 
   context 'filename is in the manifest' do
     context 'When downloaded file has a valid checksum' do
       it 'downloads a uri to a specified directory' do
-        run_download_dependency
+        run_download_dependency(file_path)
         expect(File).to exist("#{install_directory}/something.txt")
       end
 
       it 'displays the translated uri to STDOUT' do
-        stdout, _, _ = run_download_dependency
+        stdout, _, _ = run_download_dependency(file_path)
         expect(stdout.chomp).to include(file_path)
       end
 
@@ -84,12 +84,12 @@ dependencies:
         let(:manifest_url) { "file://#{credentials}@#{file_path}" }
 
         it 'does not write credentials to STDOUT' do
-          stdout, _ , _ = run_download_dependency
+          stdout, _ , _ = run_download_dependency(file_path)
           expect(stdout.chomp).not_to include(credentials)
         end
 
-        it 'it redacts credentials from STDOUT' do
-          stdout, _ , _ = run_download_dependency
+        it 'redacts credentials from STDOUT' do
+          stdout, _ , _ = run_download_dependency(file_path)
           expect(stdout.chomp).to include(redacted)
         end
       end
@@ -100,7 +100,7 @@ dependencies:
 
       it %Q{the process ends with failing exit code,
             and is '3' because of the PHP buildpack behaviour} do
-        stdout, _, status = run_download_dependency
+        stdout, _, status = run_download_dependency(file_path)
         generated_checksum = Digest::MD5.file(file_path).hexdigest
         expect(stdout.chomp).to match(/DEPENDENCY_MD5_MISMATCH for .*something\.txt: generated md5: #{Regexp.quote(generated_checksum)}, expected md5: #{Regexp.quote(md5)}/)
         expect(status.exitstatus).to eq 3
@@ -110,31 +110,27 @@ dependencies:
         let(:manifest_url) { "file://#{credentials}@#{file_path}" }
 
         it 'does not write credentials to STDOUT' do
-          stdout, _ , _ = run_download_dependency
+          stdout, _ , _ = run_download_dependency(file_path)
           expect(stdout.chomp).not_to include(credentials)
         end
 
         it 'it redacts credentials from STDOUT' do
-          stdout, _ , _ = run_download_dependency
+          stdout, _ , _ = run_download_dependency(file_path)
           expect(stdout.chomp).to include(redacted)
         end
       end
 
       it 'File is not present at the specified directory' do
-        run_download_dependency
+        run_download_dependency(file_path)
         expect(File).not_to exist("#{install_directory}/something.txt")
       end
     end
   end
 
   context 'filename is not in the manifest' do
-    let(:file_path) do
-      path = File.join(Dir.mktmpdir, 'something_else.txt')
-      FileUtils.touch(path)
-      path
-    end
+    let(:dependency_file_name) { 'something_else.txt' }
 
-    context 'the the input contains credentials' do
+    context 'the input contains credentials' do
       let(:input) { "file://#{credentials}@#{file_path}" }
 
       it 'does not write credentials to STDOUT' do
@@ -142,31 +138,31 @@ dependencies:
         expect(stdout.chomp).not_to include(credentials)
       end
 
-      it 'it redacts credentials from STDOUT' do
+      it 'redacts credentials from STDOUT' do
         stdout, _ , _ = run_download_dependency(input)
         expect(stdout.chomp).to include(redacted)
       end
     end
 
     it 'should display an error that the file is not in the manifest' do
-      stdout, _, _ = run_download_dependency
+      stdout, _, _ = run_download_dependency(file_path)
       expect(stdout).to match %r{DEPENDENCY_MISSING_IN_MANIFEST: .*\/something_else.txt}
     end
 
     it 'has a specific exit code' do
-      _, _, status = run_download_dependency
+      _, _, status = run_download_dependency(file_path)
       expect(status.exitstatus).to eq 1
     end
 
     it 'does not download the file to the specified directory' do
-      run_download_dependency
+      run_download_dependency(file_path)
       expect(File).not_to exist("#{install_directory}/something_else.txt")
     end
   end
 
   context 'download URI gives a redirect' do
     let(:manifest_url) { 'http://my.package.com/package.txt' }
-    let(:md5) { Digest::MD5.hexdigest 'blah blee' }
+    let(:md5)          { Digest::MD5.hexdigest 'blah blee' }
 
     before do
       proxy.start
@@ -183,7 +179,7 @@ dependencies:
     end
 
     it 'follows the redirect' do
-      run_download_dependency
+      run_download_dependency(file_path)
       expect(File).to exist("#{install_directory}/something.txt")
       expect(File.read("#{install_directory}/something.txt")).to eq 'blah blee'
     end
