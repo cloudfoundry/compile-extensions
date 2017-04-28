552932b27204f01e64315dcf20aac7f129e1add7
Keaty Gross <kgross@pivotal.io>
2017-04-28 15:21:53 -0400

    Fix env var checking
    [#142852033]
    
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- spec/integration/build_path_from_supply_spec.rb
+++ spec/integration/build_path_from_supply_spec.rb
@@ -26,13 +26,13 @@ describe 'build path from supply' do
         stdout, _, status = run_build_path_from_supply(deps_dir)
         expect(status.exitstatus).to eq 0
 
-        if ENV.has_value?('PATH')
+        if ENV['PATH'].to_s.length > 0
           path = "PATH=#{deps_dir}/01/bin:#{deps_dir}/00/bin:#{ENV['PATH']}"
         else
           path = "PATH=#{deps_dir}/01/bin:#{deps_dir}/00/bin"
         end
 
-        if ENV.has_value?('LD_LIBRARY_PATH')
+        if ENV['LD_LIBRARY_PATH'].to_s.length > 0
           ld_library_path = "LD_LIBRARY_PATH=#{deps_dir}/02/lib:#{deps_dir}/01/lib:#{ENV['LD_LIBRARY_PATH']}"
         else
           ld_library_path = "LD_LIBRARY_PATH=#{deps_dir}/02/lib:#{deps_dir}/01/lib"
@@ -56,7 +56,7 @@ describe 'build path from supply' do
         stdout, _, status = run_build_path_from_supply(deps_dir)
         expect(status.exitstatus).to eq 0
 
-        if ENV.has_value?('PATH')
+        if ENV['PATH'].to_s.length > 0
           path = "PATH=#{deps_dir}/01/bin:#{deps_dir}/00/bin:#{ENV['PATH']}"
         else
           path = "PATH=#{deps_dir}/01/bin:#{deps_dir}/00/bin"
@@ -81,7 +81,7 @@ describe 'build path from supply' do
         stdout, _, status = run_build_path_from_supply(deps_dir)
         expect(status.exitstatus).to eq 0
 
-        if ENV.has_value?('LD_LIBRARY_PATH')
+        if ENV['LD_LIBRARY_PATH'].to_s.length > 0
           ld_library_path = "LD_LIBRARY_PATH=#{deps_dir}/02/lib:#{deps_dir}/01/lib:#{ENV['LD_LIBRARY_PATH']}"
         else
           ld_library_path = "LD_LIBRARY_PATH=#{deps_dir}/02/lib:#{deps_dir}/01/lib"
@@ -106,19 +106,19 @@ describe 'build path from supply' do
         stdout, _, status = run_build_path_from_supply(deps_dir)
         expect(status.exitstatus).to eq 0
 
-        if ENV.has_value?('INCLUDE_PATH')
+        if ENV['INCLUDE_PATH'].to_s.length > 0
           include_path = "INCLUDE_PATH=#{deps_dir}/07/include:#{deps_dir}/04/include:#{deps_dir}/01/include:#{ENV['INCLUDE_PATH']}"
         else
           include_path = "INCLUDE_PATH=#{deps_dir}/07/include:#{deps_dir}/04/include:#{deps_dir}/01/include"
         end
 
-        if ENV.has_value?('CPATH')
+        if ENV['CPATH'].to_s.length > 0
           cpath = "CPATH=#{deps_dir}/07/include:#{deps_dir}/04/include:#{deps_dir}/01/include:#{ENV['CPATH']}"
         else
           cpath = "CPATH=#{deps_dir}/07/include:#{deps_dir}/04/include:#{deps_dir}/01/include"
         end
 
-        if ENV.has_value?('CPPPATH')
+        if ENV['CPPPATH'].to_s.length > 0
           cpppath= "CPPPATH=#{deps_dir}/07/include:#{deps_dir}/04/include:#{deps_dir}/01/include:#{ENV['CPPPATH']}"
         else
           cpppath= "CPPPATH=#{deps_dir}/07/include:#{deps_dir}/04/include:#{deps_dir}/01/include"
@@ -143,7 +143,7 @@ describe 'build path from supply' do
         stdout, _, status = run_build_path_from_supply(deps_dir)
         expect(status.exitstatus).to eq 0
 
-        if ENV.has_value?('PKG_CONFIG_PATH')
+        if ENV['PKG_CONFIG_PATH'].to_s.length > 0
           pkgconfig_path = "PKG_CONFIG_PATH=#{deps_dir}/08/pkgconfig:#{deps_dir}/05/pkgconfig:#{deps_dir}/02/pkgconfig:#{ENV['PKG_CONFIG_PATH']}"
         else
           pkgconfig_path = "PKG_CONFIG_PATH=#{deps_dir}/08/pkgconfig:#{deps_dir}/05/pkgconfig:#{deps_dir}/02/pkgconfig"
